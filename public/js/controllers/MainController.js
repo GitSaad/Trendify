04d90dbad7fa5be15ee2abb0b4837afbcaa3c8b7
Samuel <smyigzaw@gmail.com>
2016-06-11 15:18:28 -0400

    Changed tweets

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -19,50 +19,34 @@ app.controller('MainController', ['$scope', '$window', '$q', function($scope, $w
 	$scope.tweets = [
         {
             'id': '20',
-            'color': '#DB2828',
-            'user_location': {lat: 43.65422, lng: -79.3976585},
             'positiveSentiment': true
         },
         {
             'id': '21',
-            'color': '#21BA45',
-            'user_location': {lat: 43.656236, lng: -79.394746},
             'positiveSentiment': false
         },
         {
             'id': '22',
-            'color': '#21BA45',
-            'user_location': {lat: 43.652562, lng: -79.394563},
             'positiveSentiment': true
         },
         {
             'id': '23',
-            'color': '#DB2828',
-            'user_location': {lat: 43.6542342, lng: -79.3924525},
             'positiveSentiment': false
         },
         {
             'id': '25',
-            'color': '#2185D0',
-            'user_location': {lat: 43.652342, lng: -79.391345},
             'positiveSentiment': true
         },
         {
             'id': '26',
-            'color': '#DB2828',
-            'user_location': {lat: 43.655245, lng: -79.393536},
             'positiveSentiment': false
         },
         {
             'id': '29',
-            'color': '#2185D0',
-            'user_location': {lat: 43.65245, lng: -79.3934563},
             'positiveSentiment': true
         },
         {
             'id': '507185938620219395',
-            'color': '#2185D0',
-            'user_location': {lat: 43.6563535, lng: -79.3988475},
             'positiveSentiment': false
         }
 	];
